c88a90932ac32508d4245de90583fcf7fc918cea
Steven Burrell <sburrell23@gmail.com>
2024-06-06 13:21:20 -0600

    added cname in public folder

--- src/layout/Content.js
+++ src/layout/Content.js
@@ -1,7 +1,8 @@
 
 export default function Content() {
     return (
-        <>
-        </>
+        <div className="container" style={{"margin-top":"100px"}}>
+        Coming soon!
+        </div>
     );
 }
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,20 +5,23 @@ export default function Navbar() {
         <header>
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container">
-            <a className="navbar-brand" href="/">Bug Clicker</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-                <ul className="nav navbar-nav mb-2 mb-md-0">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Buy Merch</a>
-                    </li>
-                    {/* <li className="nav-item ">
-                        <a className="nav-link" href="/">Bugs!</a>
-                    </li> */}
-                </ul>
-            </div>
+                <a className="navbar-brand" href="/">
+                <i className="fa fa-bugs"></i>
+                    &nbsp;&nbsp;Bug Clicker
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+                    <ul className="nav navbar-nav mb-2 mb-md-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Buy Merch</a>
+                        </li>
+                        {/* <li className="nav-item ">
+                            <a className="nav-link" href="/">Bugs!</a>
+                        </li> */}
+                    </ul>
+                </div>
             </div>
         </nav>
         </header>
